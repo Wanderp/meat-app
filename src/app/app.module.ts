730c5d43ff1d25c168b133026f04c29596c3ede0
Wanderpaulo Lazaro da Silva Santos <wanderpaulo.santos@corregedoria.df>
2018-01-17 17:03:27 -0200

    Reviews, menu e shopping-cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,8 @@ import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart-service';
 
 @NgModule({
   declarations: [
@@ -40,7 +41,7 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
     RouterModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService],
+  providers: [RestaurantsService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
