617d87d7f2f5a5db4162ee36fb63e6558017bf98
Wanderpaulo Lazaro da Silva Santos <wanderpaulo.santos@corregedoria.df>
2018-02-05 12:40:47 -0200

    Finalizando a compra.

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,11 +1,16 @@
 import {Injectable} from '@angular/core'
 import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-cart-service'
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
+import {Http, Headers, RequestOptions} from '@angular/http'
+import {Observable} from 'rxjs/Observable'
+import {Order, OrderItem} from './order.model'
+import 'rxjs/add/operator/map'
+import {MEAT_API} from 'app/app.api'
 
 @Injectable()
 export class OrderService{
 
-    constructor(private cartService: ShoppingCartService){
+    constructor(private cartService: ShoppingCartService, private http: Http){
     }
 
     cartItems(): CartItem[]{
@@ -26,5 +31,18 @@ export class OrderService{
 
     itemsValue(): number{
         return this.cartService.total()
+    }
+
+    clear(){
+        this.cartService.clear()
+    }
+
+    checkOrder(order: Order): Observable<string> {
+        const headers = new Headers()
+        headers.append('Content-Type', 'application/json')
+        return this.http.post(`${MEAT_API}/orders`,
+         JSON.stringify(order),
+        new RequestOptions({headers: headers}))
+        .map(response => response.json())
     }
 }
